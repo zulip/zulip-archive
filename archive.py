f4f9e67e6e19c6c9f54cdfab7ffae89c3b10e86d
Rob Lewis <rob.y.lewis@gmail.com>
2019-07-01 16:24:58 +0200

    cleaning, refactor

--- archive.py
+++ archive.py
@@ -1,36 +1,185 @@
 # requires python 3
 #
-# The workflow (timing on my laptop):
+# The workflow (timing for the leanprover Zulip chat, on my slow laptop):
 # - populate_all() builds a json file in `json_root` for each topic, containing message data,
 #   and an index json file mapping streams to their topics.
 #   This uses the Zulip API and takes ~10 minutes to crawl the whole chat.
 # - populate_incremental() assumes there is already a json cache and collects only new messages.
-# - write_markdown() builds markdown files in `md_index` from the json. This takes ~15 seconds.
+# - write_markdown() builds markdown files in `md_root` from the json. This takes ~15 seconds.
 # - This markdown can be pushed directly to GitHub or built locally with `jekyll serve --incremental`.
 #   Building locally takes about 1 minute.
 #
-# The json format for stream_index.md is:
+# The json format for stream_index.json is:
 # { 'time': the last time stream_index.md was updated,
 #   'streams': { stream_name: { 'id': stream_id,
 #                               'latest_id': id of latest post in stream,
 #                               'topic_data': { topic_name: { topic_size: num posts in topic,
 #                                                            latest_date: time of latest post }}}}}
+#
+# stream_index.json is created in the json_root directory.
+# This directory also contains a subdirectory for each archived stream.
+# In each stream subdirectory, there is a json file for each topic in that stream.
+# This json file is a list of message objects, as desribed at https://zulipchat.com/api/get-messages
+#
 
 from datetime import date, datetime
 from pathlib import Path
 from zlib import adler32
 import zulip, string, os, time, json, urllib, argparse, subprocess
 
-json_root = Path("./_json")
-md_root = Path("archive")
-md_index = Path("index.md")
-html_root = Path("archive")
-last_updated_path = Path("_includes/archive_update.html")
-site_url = "https://leanprover-community.github.io/"
+## Configuration options
+
+json_root = Path("./_json") # directory to store the generated .json files
+md_root = Path("archive") # directory to store the generated .md and .html files
+md_index = Path("index.md") # name for the index files
+html_root = Path("archive") # user-facing path for the index
+last_updated_dir = Path("_includes")# directory to store update timestamp
+last_updated_file = Path("archive_update.html") # filename for update timestamp
+
+site_url = "https://leanprover-community.github.io/" # user-facing root url. Only needed for md/html generation.
+zulip_url = "https://leanprover.zulipchat.com/" # Zulip chat url. Only needed for md/html generation
+
+# Streams in stream_blacklist are ignored.
+# If stream_whitelist is nonempty, only streams that appear there and not in
+# stream_blacklist will be archived.
 stream_blacklist = ['rss', 'travis', 'announce']
+stream_whitelist = []
 
 # config_file should point to a Zulip api config
-client = zulip.Client(config_file="./.zuliprc")
+client = zulip.Client(config_file="./zuliprc")
+
+
+## Customizable display functions.
+
+# When generating displayable md/html, we create the following structure inside md_root:
+# * md_root/md_index displays a list of all streams
+# * for each stream str, md_root/str/md_index displays a list of all topics in str
+# * for each topic top in a stream str, md_root/str/top.html displays the posts in top.
+#
+# Some sanitization is needed to ensure that urls are unique and acceptable.
+# Use sanitize_stream(stream_name, stream_id) in place of str above.
+# Use sanitize_topic(topic_name) in place of top.
+#
+# The topic display must be an html file, since we use the html provided by Zulip.
+# The index pages are generated in markdown by default, but this can be changed to html.
+# The default settings are designed for a Jekyll build.
+
+# writes the Jekyll header info for the index page listing all streams.
+def write_stream_index_header(outfile):
+    outfile.writelines(['---\n', 'layout: archive\n', 'title: Lean Prover Zulip Chat Archive\n'])
+    outfile.write('permalink: {}/index.html\n'.format(html_root))
+    outfile.writelines(['---\n\n','---\n\n','## Streams:\n\n'])
+
+# writes the index page listing all streams.
+# `streams`: a dict mapping stream names to stream json objects as described in the header.
+def write_stream_index(streams):
+    outfile = open_outfile(md_root, md_index, 'w+')
+    write_stream_index_header(outfile)
+    for s in sorted(streams, key=lambda s: len(streams[s]['topic_data']), reverse=True):
+        num_topics = len(streams[s]['topic_data'])
+        outfile.write("* [{0}]({1}/index.html) ({2} topic{3})\n\n".format(
+            s,
+            sanitize_stream(s, streams[s]['id']),
+            num_topics,
+            '' if num_topics == 1 else 's'))
+    outfile.write('\n{% include ' + str(last_updated_file) + ' %}')
+    outfile.close()
+
+# writes the Jekyll header info for the index page for a given stream.
+def write_topic_index_header(outfile, stream_name, stream):
+    permalink = 'permalink: {1}/{0}/index.html'.format(
+        sanitize_stream(stream_name, stream['id']), html_root
+    )
+    strm = '## Stream: [{0}]({1}/index.html)'.format(
+        stream_name, format_stream_url(stream['id'], stream_name)
+    )
+    outfile.writelines(['---\n', 'layout: archive\n', 'title: Lean Prover Zulip Chat Archive\n',
+                        permalink, '\n---\n\n', strm, '\n---\n\n', '### Topics:\n\n'])
+
+# writes an index page for a given stream, printing a list of the topics in that stream.
+# `stream_name`: the name of the stream.
+# `stream`: a stream json object as described in the header
+def write_topic_index(stream_name, stream):
+    directory = md_root / Path(sanitize_stream(stream_name, stream['id']))
+    outfile = open_outfile(directory, md_index, 'w+')
+    write_topic_index_header(outfile, stream_name, stream)
+    for topic_name in sorted(stream['topic_data'], key=lambda tn: stream['topic_data'][tn]['latest_date'], reverse=True):
+        t = stream['topic_data'][topic_name]
+        outfile.write("* [{0}]({1}.html) ({2} message{4}, latest: {3})\n\n".format(
+            escape_pipes(topic_name),
+            sanitize_topic(topic_name),
+            t['size'],
+            datetime.utcfromtimestamp(t['latest_date']).strftime('%b %d %Y at %H:%M'),
+            '' if t['size'] == 1 else 's'
+        ))
+    outfile.write('\n{% include ' + str(last_updated_file) + ' %}')
+    outfile.close()
+
+# formats the header for a topic page.
+def write_topic_header(outfile, stream_name, stream_id, topic_name):
+    permalink = 'permalink: {0}/{1}/{2}.html'.format(
+        html_root,
+        sanitize_stream(stream_name, stream_id),
+        sanitize_topic(topic_name)
+    )
+    strm = '<h2>Stream: <a href="{1}/index.html">{0}</a>'.format(
+        stream_name,
+        format_stream_url(stream_id, stream_name)
+    )
+    tpc = '<h3>Topic: <a href="{2}/{1}.html">{0}</a></h3>'.format(
+        topic_name,
+        sanitize_topic(topic_name),
+        format_stream_url(stream_id, stream_name)
+    )
+    outfile.writelines(['---\n', 'layout: archive\n', 'title: Lean Prover Zulip Chat Archive\n',
+                        permalink, '\n---\n\n', strm, '\n', tpc, '\n\n<hr>\n\n', '<base href="https://leanprover.zulipchat.com">\n'])
+
+# formats a single post in a topic
+# Note: the default expects the Zulip "Z" icon at site_url+'assets/img/zulip2.png'
+def format_message(user_name, date, msg, link, anchor_name, anchor_url):
+    anchor = '<a name="{0}"></a>'.format(anchor_name)
+    zulip_link = '<a href="{0}" class="zl"><img src="{1}" alt="view this post on Zulip"></a>'.format(link, site_url+'assets/img/zulip2.png')
+    local_link = '<a href="{0}">{1} ({2})</a>'.format(anchor_url, user_name, date)
+    return '{0}\n<h4>{1} {2}:</h4>\n{3}'.format(anchor, zulip_link, local_link, msg)
+
+
+# writes the body of a topic page (ie, a list of messages)
+# `messages`: a list of message json objects, as defined in the Zulip API
+def write_topic_body(messages, stream_name, stream_id, topic_name, outfile):
+    for c in messages:
+        name = c['sender_full_name']
+        date = datetime.utcfromtimestamp(c['timestamp']).strftime('%b %d %Y at %H:%M')
+        msg = c['content']
+        link = structure_link(stream_id, stream_name, topic_name, c['id'])
+        anchor_name = str(c['id'])
+        anchor_link = '{0}{4}/{1}/{2}.html#{3}'.format(
+            site_url,
+            sanitize_stream(stream_name, stream_id),
+            sanitize_topic(topic_name),
+            anchor_name,
+            html_root)
+        outfile.write(format_message(name, date, msg, link, anchor_name, anchor_link))
+        outfile.write('\n\n')
+
+
+# writes a topic page.
+# `stream`: a stream json object as defined in the header
+def write_topic(stream_name, stream, topic_name):
+    json_path = json_root / Path(sanitize_stream(stream_name, stream['id'])) / Path (sanitize_topic(topic_name) + '.json')
+    f = json_path.open('r', encoding='utf-8')
+    messages = json.load(f)
+    f.close()
+    o = open_outfile(md_root / Path(sanitize_stream(stream_name, stream['id'])), Path(sanitize_topic(topic_name) + '.html'), 'w+')
+    write_topic_header(o, stream_name, stream['id'], topic_name)
+    o.write('\n{% raw %}\n')
+    write_topic_body(messages, stream_name, stream['id'], topic_name, o)
+    o.write('\n{% endraw %}\n')
+    o.write('\n{% include ' + str(last_updated_file) + ' %}')
+    o.close()
+
+
+
+## Nothing after this point should need to be modified.
 
 ## String cleaning functions
 
@@ -51,6 +200,7 @@ def sanitize_stream(stream_name, stream_id):
 def escape_pipes(s):
     return s.replace('|','\|').replace(']','\]').replace('[','\[')
 
+
 ## retrieve information from Zulip
 
 # runs client.cmd(args). If the response is a rate limit error, waits the requested time and tries again.
@@ -65,7 +215,7 @@ def safe_request(cmd, args):
 # Safely open dir/filename, creating dir if it doesn't exist
 def open_outfile(dir, filename, mode):
     if not dir.exists():
-        dir.mkdir()
+        os.makedirs(str(dir))
     return (dir / filename).open(mode, encoding='utf-8')
 
 # Retrieves all messages matching request from Zulip, starting at post id anchor.
@@ -93,6 +243,9 @@ def separate_results(list):
             map[m['subject']].append(m)
     return map
 
+def test_valid(s):
+    return s['name'] not in stream_blacklist and (True if stream_whitelist == [] else s['name'] in stream_whitelist)
+
 # Retrieves only new messages from Zulip, based on timestamps from the last update.
 # Raises an exception if there is no index at json_root/stream_index.json
 def populate_incremental():
@@ -103,7 +256,7 @@ def populate_incremental():
     f = stream_index.open('r', encoding='utf-8')
     stream_index = json.load(f, encoding='utf-8')
     f.close()
-    for s in (s for s in streams if s['name'] not in stream_blacklist):
+    for s in (s for s in streams if test_valid(s)):
         print(s['name'])
         if s['name'] not in stream_index['streams']:
             stream_index['streams'][s['name']] = {'id':s['stream_id'], 'latest_id':0, 'topic_data':{}}
@@ -138,7 +291,7 @@ def populate_incremental():
 def populate_all():
     streams = safe_request(client.get_streams, [])['streams']
     ind = {}
-    for s in (s for s in streams if s['name'] not in stream_blacklist):
+    for s in (s for s in streams if test_valid(s)):
         print(s['name'])
         topics = safe_request(client.get_stream_topics, [s['stream_id']])['topics']
         nind = {'id': s['stream_id'], 'latest_id':0}
@@ -167,112 +320,22 @@ def populate_all():
 
 
 
-
 ## Display
 
 # Create a link to a post on Zulip
 def structure_link(stream_id, stream_name, topic_name, post_id):
     sanitized = urllib.parse.quote(
         '{0}-{1}/topic/{2}/near/{3}'.format(stream_id, stream_name, topic_name, post_id))
-    return 'https://leanprover.zulipchat.com/#narrow/stream/' + sanitized
+    return zulip_url + '#narrow/stream/' + sanitized
 
 # absolute url of a stream directory
 def format_stream_url(stream_id, stream_name):
     return site_url + str(html_root) + '/' + sanitize_stream(stream_name, stream_id)
 
-# formats a single post
-def format_message(name, date, msg, link, anchor_name, anchor_url):
-    #return u'<a name="{4}"></a>\n #### [![view this post on Zulip]({5}/assets/img/zulip2.png)]({3}) [ {0} ({1})]({6}):\n{2}'.format(name, date, msg, link, anchor_name, site_url, anchor_url)
-    anchor = '<a name="{0}"></a>'.format(anchor_name)
-    zulip_link = '<a href="{0}" class="zl"><img src="{1}" alt="view this post on Zulip"></a>'.format(link, site_url+'assets/img/zulip2.png')
-    local_link = '<a href="{0}">{1} ({2})</a>'.format(anchor_url, name, date)
-    return '{0}\n<h4>{1} {2}:</h4>\n{3}'.format(anchor, zulip_link, local_link, msg)
-    #return u'<a name="{4}"></a>\n #### [![view this post on Zulip]({5}/assets/img/zulip2.png)]({3}) [ {0} ({1})]({6}):\n{2}'.format(name, date, msg, link, anchor_name, site_url, anchor_url)
-
-# writes the body of a topic page (ie, a list of messages)
-def write_topic(messages, stream_name, stream_id, topic_name, outfile):
-    for c in messages:
-        name = c['sender_full_name']
-        date = datetime.utcfromtimestamp(c['timestamp']).strftime('%b %d %Y at %H:%M')
-        msg = c['content']
-        link = structure_link(stream_id, stream_name, topic_name, c['id'])
-        anchor_name = str(c['id'])
-        anchor_link = '{0}{4}/{1}/{2}.html#{3}'.format(
-            site_url,
-            sanitize_stream(stream_name, stream_id),
-            sanitize_topic(topic_name),
-            anchor_name,
-            html_root)
-        outfile.write(format_message(name, date, msg, link, anchor_name, anchor_link))
-        outfile.write('\n\n')
-
-# writes an index page for a given stream, printing a list of the topics in that stream.
-# `s_name`: the name of the stream. `s`: a json object as described in the header
-def write_topic_index(s_name, s):
-    directory = md_root / Path(sanitize_stream(s_name, s['id']))
-    outfile = open_outfile(directory, md_index, 'w+')
-    header = ("---\nlayout: archive\ntitle: Lean Prover Zulip Chat Archive\npermalink: {2}/{1}/index.html\n---\n\n" +
-            "## Stream: [{0}]({3}/index.html)\n\n---\n\n### Topics:\n\n").format(
-                s_name,
-                sanitize_stream(s_name, s['id']),
-                html_root,
-                format_stream_url(s['id'], s_name))
-    outfile.write(header)
-    for topic_name in sorted(s['topic_data'], key=lambda tn: s['topic_data'][tn]['latest_date'], reverse=True): #s['topic_data']:
-        t = s['topic_data'][topic_name]
-        outfile.write("* [{0}]({1}.html) ({2} message{4}, latest: {3})\n\n".format(
-            escape_pipes(topic_name),
-            sanitize_topic(topic_name),
-            t['size'],
-            datetime.utcfromtimestamp(t['latest_date']).strftime('%b %d %Y at %H:%M'),
-            '' if t['size'] == 1 else 's'
-        ))
-    outfile.write('\n{% include archive_update.html %}')
-    outfile.close()
-
-# writes the index page listing all streams.
-# `streams`: a dict mapping stream names to stream json objects as described in the header.
-def write_stream_index(streams):
-    outfile = (md_root / md_index).open('w+', encoding='utf-8')
-    outfile.write("---\nlayout: archive\ntitle: Lean Prover Zulip Chat Archive\npermalink: {}/index.html\n---\n\n---\n\n## Streams:\n\n".format(html_root))
-    for s in sorted(streams, key=lambda s: len(streams[s]['topic_data']), reverse=True):
-        num_topics = len(streams[s]['topic_data'])
-        outfile.write("* [{0}]({1}/index.html) ({2} topic{3})\n\n".format(
-            s,
-            sanitize_stream(s, streams[s]['id']),
-            num_topics,
-            '' if num_topics == 1 else 's'))
-    outfile.write('\n{% include archive_update.html %}')
-    outfile.close()
-
-# formats the header for a topic page.
-def format_topic_header(stream_name, stream_id, topic_name):
-    return ("---\nlayout: archive\ntitle: Lean Prover Zulip Chat Archive \npermalink: {4}/{2}/{3}.html\n---\n\n" +
-            '<h2>Stream: <a href="{5}/index.html">{0}</a>\n<h3>Topic: <a href="{5}/{3}.html">{1}</a></h3>\n\n<hr>\n\n<base href="https://leanprover.zulipchat.com">').format(
-                stream_name,
-                topic_name,
-                sanitize_stream(stream_name, stream_id),
-                sanitize_topic(topic_name),
-                html_root,
-                format_stream_url(stream_id, stream_name))
-
-# writes a topic page.
-def get_topic_and_write(stream_name, stream, topic):
-    json_path = json_root / Path(sanitize_stream(stream_name, stream['id'])) / Path (sanitize_topic(topic) + '.json')
-    f = json_path.open('r', encoding='utf-8')
-    messages = json.load(f)
-    f.close()
-    o = open_outfile(md_root / Path(sanitize_stream(stream_name, stream['id'])), Path(sanitize_topic(topic) + '.html'), 'w+')
-    o.write(format_topic_header(stream_name, stream['id'], topic))
-    o.write('\n{% raw %}\n')
-    write_topic(messages, stream_name, stream['id'], topic, o)
-    o.write('\n{% endraw %}\n')
-    o.close()
-
 # updates the "last updated" footer message to time `t`.
 def write_last_updated(t):
-    f = last_updated_path.open('w+')
-    f.write('<p>Last updated: {} UTC</p>'.format(t))
+    f = open_outfile(last_updated_dir, last_updated_file, 'w+')
+    f.write('<hr><p>Last updated: {} UTC</p>'.format(t))
     f.close()
 
 # writes all markdown files to md_root, based on the archive at json_root.
@@ -281,13 +344,15 @@ def write_markdown():
     stream_info = json.load(f, encoding='utf-8')
     f.close()
     streams = stream_info['streams']
-    write_last_updated(str(stream_info['time'])) #(datetime.utcfromtimestamp(time.time()))
+    write_last_updated(str(stream_info['time']))
     write_stream_index(streams)
     for s in streams:
         print('building: ', s)
         write_topic_index(s, streams[s])
         for t in streams[s]['topic_data']:
-            get_topic_and_write(s, streams[s], t)
+            write_topic(s, streams[s], t)
+
+
 
 # resets the current repository to match origin/master
 def github_pull():
