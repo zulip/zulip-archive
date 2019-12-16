9b3d180c9abefe662ba06bc35e2f222d1f8b5b23
Steve Howell <showell30@yahoo.com>
2019-12-16 16:03:58 -0500

    Split out github.py and hosting.md.
    I want to decouple the hosting considerations from the
    main archive.py script, since there are many ways that
    folks might serve up content, and then the `-f` option
    in particular here is kinda confusing and dangerous.

---
+++ github.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+WARNING!
+
+This script is no longer supported, but it includes example code
+from one of our customers who used this repo to store the Zulip
+content as well as the code.  We recommend to most folks to
+create a **separate** repo for your content, even if you are
+using Github to serve the content.
+"""
+
+from datetime import datetime
+import time, argparse, subprocess
+
+parser = argparse.ArgumentParser(description='Push/pull repo.')
+
+# resets the current repository to match origin/master
+def github_pull():
+    print(subprocess.check_output(['git','fetch','origin','master']))
+    print(subprocess.check_output(['git','reset','--hard','origin/master']))
+
+# commits changes in archive/ and pushes the current repository to origin/master
+def github_push():
+    print(subprocess.check_output(['git','add','archive/*']))
+    print(subprocess.check_output(['git','add','_includes/archive_update.html']))
+    print(subprocess.check_output(['git','commit','-m','auto update: {}'.format(datetime.utcfromtimestamp(time.time()).strftime('%b %d %Y at %H:%M UTC'))]))
+    print(subprocess.check_output(['git','push']))
+
+parser.add_argument('-f', action='store_true', default=False, help='Pull from GitHub before updating. (Warning: could overwrite this script.)')
+parser.add_argument('-p', action='store_true', default=False, help='Push results to GitHub.')
+
+if results.f:
+    github_pull()
+if results.p:
+    github_push()
---
+++ hosting.md
@@ -0,0 +1,35 @@
+The code in this repo helps you extract content from a Zulip
+instance and build the basic HTML structure, but it leaves it
+to you to actually host the data on some server.
+
+In other words, Zulip is not opinionated about you serve
+the HTML (and in some ways the entire mission of this project
+is to empower you to put your data where you want).
+
+Here are some convenient options for serving the HTML:
+
+### Jekyll
+
+[Jekyll](https://jekyllrb.com/) is a popular tool for
+serving static content.
+
+The archive script generates index pages in markdown that are
+designed for a Jekyll build, so you can do something like this:
+
+```
+jekyll serve --incremental
+```
+
+### GitHub
+
+You can use GitHub Pages to serve your HTML.  We recommend
+configuring your `md_root` to point into your local copy of
+your `username.github.io` repo (e.g. `alice.github.io`) and
+then push from there.
+
+Some customers will want to just use the same repo for both
+this tooling and the content from their Zulip instance.  We
+don't recommend this approach long term, since it can complicate
+staying up to date with patches from this repo.  If you are
+still interested in this approach, despite the warnings, you
+may find the `github.py` script to be useful.
