0f42e7b1cc58df62ce88c706bd1cd5313452a592
Wim Looman <git@nemo157.com>
2020-08-31 10:37:14 +0200

    Specify that charset of pages is utf-8

--- archive.py
+++ archive.py
@@ -105,7 +105,7 @@ def write_topic_index(stream_name, stream):
 
 # formats the header for a topic page.
 def write_topic_header(outfile, stream_name, stream_id, topic_name):
-    outfile.write('<head><link href="/style.css" rel="stylesheet"></head>')
+    outfile.write('<head><meta charset="utf-8"><link href="/style.css" rel="stylesheet"></head>')
     permalink = 'permalink: {0}/{1}/{2}.html'.format(
         html_root,
         sanitize_stream(stream_name, stream_id),
