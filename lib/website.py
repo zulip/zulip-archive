ad2d714218639b10a443cce4da02c73c5a2be9a0
tison <wander4096@gmail.com>
2023-10-03 17:42:22 +0800

    dirs_exist_ok=True
    Signed-off-by: tison <wander4096@gmail.com>

--- lib/website.py
+++ lib/website.py
@@ -110,10 +110,11 @@ def build_website(
                 page_footer_html,
             )
 
-    # Copy the entire content of <repo_root>/assets into md_root.
-    # We use copy_tree from distutils instead of shutil.copytree so that it
-    # doesn't raise an error when assets/ already exists inside the md_root.
-    copytree(str(Path(repo_root) / "assets"), str(Path(md_root) / "assets"))
+    copytree(
+        str(Path(repo_root) / "assets"),
+        str(Path(md_root) / "assets"),
+        dirs_exist_ok=True,
+    )
 
     # Copy .nojekyll into md_root as well.
     copyfile(str(Path(repo_root) / ".nojekyll"), str(Path(md_root) / ".nojekyll"))
